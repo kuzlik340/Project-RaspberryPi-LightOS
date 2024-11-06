2866ea39af65420b0f053de64a8e341ec2cf3039
Timofei Kuzin <timkkuz@gmail.com>
2024-11-06 21:25:20 +0100

    Was added and tested interrupt module for hardware. Currently working fine with the timer

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 #include "print.h"
 #include "debug.h"
 #include "lib.h"
+#include "handler.h"
 
 void KMain(void)
 {
     uint64_t value = 238;
     init_uart();
     printk("Hello, Raspberry pi\r\n");
-    printk("test number %d\r\n", value);
-    printk("test number %x\r\n", value);
     printk("We are at EL %u\r\n", (uint64_t)get_el());
+
+    init_timer();
+    init_interrupt_controller();
+    enable_irq();
     while (1) {
     }
 }
