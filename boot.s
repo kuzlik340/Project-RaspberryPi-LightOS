7fcd9b244df2a9ab850ed2ddd11fd00e8a09d611
Timofei Kuzin <timkkuz@gmail.com>
2024-11-06 15:13:53 +0100

    Added comments

--- boot.s
+++ boot.s
@@ -17,20 +17,27 @@ end:
     b end
 
 kernel_entry:
+    /* check current exception level */
     mrs currentel
     lsr x0, x0, #2
+    /* if exception level is not 2 then block booting */
     cmp x0, #2
     bne end
 
+    /* init the EL1 with 0's */
     msr sctlr_el1, xzr
+    /* set 31 bit as it will boot EL1 in aarch64 mode */
     mov x0, #(1 << 31)
     msr hcr_el2, x0
+    /* turning off interrupts, setting EL1 to execute, set aarch64 mode */
     mov x0, #0b1111000101
     msr spsr_el2, x0
-
+    /* set an address of el1_entry into x0 reg */
     adr x0, el1_entry
+    /* seeting address of instruction that has to be executed
+     * after leaving EL2 */
     msr elr_el2, x0
-
+    /* go to EL1 */
     eret
 
 el1_entry:    
--- debug.h
+++ debug.h
@@ -1,9 +1,14 @@
+/* This module is for debugging OS while creating, 
+ * will be turned off in version 0.1*/
+
+
 #ifndef _DEBUG_H
 #define _DEBUG_H
 
 #include "stdint.h"
 
-#define ASSERT(e) do {                           \
+/* for example we can check buffer != NULL */
+#define ASSERT(e) do {                          \
     if (!(e))                                   \
        error_check(__FILE__, __LINE__);         \
 } while(0)                                       
