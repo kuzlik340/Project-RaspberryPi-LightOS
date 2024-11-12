448af2f6b2c3fb6937a539caa04f2666972190fe
Timofei Kuzin <timkkuz@gmail.com>
2024-11-12 23:04:27 +0100

    Added comments to main.c

--- kernel/main.c
+++ kernel/main.c
@@ -1,3 +1,10 @@
+/*
+ * main.c   v1.0
+ *
+ * This is the kernel of OS.
+ *
+ * T.Kuz    11.2024
+ */
 #include "uart.h"
 #include "print.h"
 #include "debug.h"
@@ -8,7 +15,7 @@ void KMain(void)
 {
     uint64_t value = 238;
     init_uart();
-    printk("Hello, Raspberry pi\r\n");
+    printk("Hello from Raspberry pi\r\n");
     printk("We are at EL %u\r\n", (uint64_t)get_el());
 
     init_timer();
